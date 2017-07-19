477eb80ba9dcf02128f9c33d1c59f3cd7a5f8216
Abel Lifaefi Mbula <bam92@debian.abelmbula.fr>
2017-07-19 15:16:59 +0200

    page doctors added

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,7 @@
 $app->get('/', function () use ($app) {
     return $app['twig']->render('home.html.twig');
 })->bind('home');
+
+$app->get('/doctors', function () use ($app) {
+    return $app['twig']->render('doctors.html.twig');
+})->bind('doctors');
